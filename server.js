c68c6f5836872252d44aa5683e347159360ed272
BryantErwin <bryanterwin14@gmail.com>
2019-10-30 13:34:02 -0400

    2

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
+const morgan = require('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
@@ -16,11 +17,27 @@ function a (req, res, next) {
   next();
 }
 
+function gateKeeper(req, res, next){
+  // new way of reading data sent by the client
+  const password = req.headers.password || '';
+
+  if(password.toLowerCase() === 'mellon') {
+    next()
+  }
+  if(password === ''){
+    res.status(400).json({message:"Please provide a password"})
+  }else{
+    res.status(401).json({you: 'cannot pass!!'})
+  }
+}
+
 // global middleware
+server.use(gateKeeper);
 server.use(helmet());// third party
 server.use(express.json());// built-in
 server.use(dateLogger); // custom middleware
 server.use(a);
+server.use(morgan('dev'));
 
 
 server.use('/api/hubs', hubsRouter);
